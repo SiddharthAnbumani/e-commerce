5a60d2da6fb8bbacd2b64f2db3408a74d43fbb13
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-04-01 11:32:18 +0530

    Added Cart items

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,7 +1,28 @@
+import CartItem from "../components/CartItem";
+
+
 export default function Cart(){
     return (
-        <div className="">
-            <h1 className="text-5xl font-extrabold">This is cart element</h1>
+        <div className="flex ">
+            <div className="h-screen  w-8/12">
+            {/* empty Container */}
+            </div>
+            <div className="h-full w-4/12 bg-white rounded-4xl mr-3">
+            <h1 className="text-5xl font-extrabold text-center my-3">Cart</h1>
+            <div className="flex flex-col">
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+            </div>
+            </div>
         </div>
     )
 }
